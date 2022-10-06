e75d1215024f76047c8453915666175eafe3503a
Rahul Sharma <71009337+rs-rahul@users.noreply.github.com>
2022-10-06 20:02:36 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Assignment of DevOps Part 2 CI CD for ALB
+          Assignment of DevOps Part 2 CI CD for ALB. Completed :D
         </p>
 	<p>
 	  Automating things can make life easier.
